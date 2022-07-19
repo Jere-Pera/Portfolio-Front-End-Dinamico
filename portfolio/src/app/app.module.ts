48c74dc794803e36657bd99c51d42ed35c538649
jeremias <peraltajeremias03@gmail.com>
2022-07-19 11:09:22 -0300

    Añadido de componentes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExpEduComponent } from './components/exp-edu/exp-edu.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExpEduComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    
   ],
   imports: [
     BrowserModule
