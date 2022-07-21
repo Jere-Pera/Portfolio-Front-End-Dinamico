890b3121a8cd5fb2d28c9cc1ddef23803bab0e0a
jeremias <peraltajeremias03@gmail.com>
2022-07-21 10:18:34 -0300

    Problema de instancia del databinding con ell arreglo del JSON en modulo proyectos solucionado

--- portfolio/src/app/components/proyectos/proyectos.component.ts
+++ portfolio/src/app/components/proyectos/proyectos.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { DatosService } from 'src/app/servicios/datos.service';
 
+
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
+
+
 export class ProyectosComponent implements OnInit {
-  proyects: any;
+  proyList: any;             
   constructor(private datosPorfolio: DatosService) { }
 
   ngOnInit(): void {
     this.datosPorfolio.obtenerDatos().subscribe(data => {
-      this.proyects = data;
+      this.proyList = data.proyectos;
     })
   }
 
